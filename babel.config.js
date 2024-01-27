91ad0530540540407ffd5534f5381018ba19dacd
8leamnk <silkviewrk@gmail.com>
2024-01-27 23:21:13 +0900

    feat: babel-plugin-styled-components 설치 및 적용
    추가 내용:
    - styled-components가 적용되지 않은 상태로 렌더링되도록 설정
    - warning: prop `classname` did not match 경고를 해결

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,10 @@
 const babelConfig = {
   presets: [
+    'next/babel',
     ['@babel/preset-env', { targets: { node: 'current' } }],
     '@babel/preset-typescript',
   ],
+  plugins: ['babel-plugin-styled-components'],
 };
 
 module.exports = babelConfig;
