4b43dd954536063f9ad8ed68580705388d7ed536
8leamnk <silkviewrk@gmail.com>
2024-01-31 18:48:50 +0900

    refactor: 팝업 정보 받아오는 기능 변경
    변경 내용:
    - 타입 변경
    - reject 안에 에러 객체를 사용

--- src/constants/dummyData.ts
+++ src/constants/dummyData.ts
@@ -1,16 +1,18 @@
-import { PopupInner } from './types';
+import { PopupInfo } from './types';
 
-const popupData: PopupInner = {
+const ERROR_MESSAGE: string = '팝업 정보를 받아오는 중 에러가 발생했습니다.';
+
+const popupData: PopupInfo = {
   title: '팝업 타이틀',
   content: '팝업 내용',
 };
 
-function getPopupData(): Promise<PopupInner | null> {
+function getPopupData(): Promise<PopupInfo> {
   return new Promise((resolve, reject) => {
     if (popupData) {
       resolve(popupData);
     } else {
-      reject();
+      reject(new Error(ERROR_MESSAGE));
     }
   });
 }
