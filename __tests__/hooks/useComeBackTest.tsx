c44954b62d8b979cdf088dada5c4d44d6cdd4837
8leamnk <silkviewrk@gmail.com>
2024-02-15 14:05:38 +0900

    test: 목표 페이지로 돌아오는지 확인하는 기능 리팩토링한 것 테스트

--- __tests__/hooks/useComeBackTest.tsx
+++ __tests__/hooks/useComeBackTest.tsx
@@ -2,59 +2,45 @@
 
 import { renderHook } from '@testing-library/react';
 import useComeBack from '@/hooks/useComeBack';
-import { HistoryInfo, HistoryType } from '@/constants/types';
+import { HistoryInfo } from '@/constants/types';
 
 describe('목표 페이지로 돌아오는지 확인하는 기능 테스트', () => {
-  interface ComeBackProps {
-    historyInfo: HistoryInfo;
-    targetPathname: string;
+  interface ComeBackProps<T> {
+    historyInfo: HistoryInfo<T>;
+    targetPathname: T;
   }
 
   const TARGET_PATHNAME: string = '/target';
   const ANOTHER_PATHNAME: string = '/another-page';
 
-  test.each([
-    [
-      {
-        history: [TARGET_PATHNAME],
-        prevPathname: undefined,
-        currPathname: TARGET_PATHNAME,
-      },
-      false,
-    ],
-    [
-      {
-        history: [ANOTHER_PATHNAME],
-        prevPathname: undefined,
-        currPathname: ANOTHER_PATHNAME,
-      },
-      false,
-    ],
-  ])(
-    '직전 경로가 없으면 무조건 false를 반환한다.',
-    (historyInfo: HistoryInfo, output: boolean) => {
-      // when
-      const { result } = renderHook(
-        (props: ComeBackProps) => useComeBack(props),
-        {
-          initialProps: { historyInfo, targetPathname: TARGET_PATHNAME },
-        },
-      );
+  test('직전 경로가 없으면 무조건 false를 반환한다.', () => {
+    // given
+    const HISTORY_INFO: HistoryInfo<string> = {
+      history: { first: null, last: null, size: 0 },
+      prevPathname: undefined,
+      currPathname: TARGET_PATHNAME,
+    };
+    const OUTPUT: boolean = false;
 
-      // then
-      expect(result.current).toBe(output);
-    },
-  );
+    // when
+    const { result } = renderHook(
+      (props: ComeBackProps<string>) => useComeBack<string>(props),
+      {
+        initialProps: {
+          historyInfo: HISTORY_INFO,
+          targetPathname: TARGET_PATHNAME,
+        },
+      },
+    );
+
+    // then
+    expect(result.current).toBe(OUTPUT);
+  });
 
   test('직전 경로가 존재할 때, 현재 경로가 목표 페이지가 아닌 경우에는 false를 반환한다.', () => {
     // given
-    const HISTORY: HistoryType = [
-      ANOTHER_PATHNAME,
-      TARGET_PATHNAME,
-      ANOTHER_PATHNAME,
-    ];
-    const HISTORY_INFO = {
-      history: HISTORY,
+    const HISTORY_INFO: HistoryInfo<string> = {
+      history: { first: null, last: null, size: 0 },
       prevPathname: TARGET_PATHNAME,
       currPathname: ANOTHER_PATHNAME,
     };
@@ -62,7 +48,7 @@ describe('목표 페이지로 돌아오는지 확인하는 기능 테스트', ()
 
     // when
     const { result } = renderHook(
-      (props: ComeBackProps) => useComeBack(props),
+      (props: ComeBackProps<string>) => useComeBack<string>(props),
       {
         initialProps: {
           historyInfo: HISTORY_INFO,
@@ -77,13 +63,8 @@ describe('목표 페이지로 돌아오는지 확인하는 기능 테스트', ()
 
   test('직전 경로가 존재할 때, 현재 경로가 목표 페이지일 경우에는 true를 반환한다.', () => {
     // given
-    const HISTORY: HistoryType = [
-      TARGET_PATHNAME,
-      ANOTHER_PATHNAME,
-      TARGET_PATHNAME,
-    ];
-    const HISTORY_INFO = {
-      history: HISTORY,
+    const HISTORY_INFO: HistoryInfo<string> = {
+      history: { first: null, last: null, size: 0 },
       prevPathname: ANOTHER_PATHNAME,
       currPathname: TARGET_PATHNAME,
     };
@@ -91,7 +72,7 @@ describe('목표 페이지로 돌아오는지 확인하는 기능 테스트', ()
 
     // when
     const { result } = renderHook(
-      (props: ComeBackProps) => useComeBack(props),
+      (props: ComeBackProps<string>) => useComeBack<string>(props),
       {
         initialProps: {
           historyInfo: HISTORY_INFO,
