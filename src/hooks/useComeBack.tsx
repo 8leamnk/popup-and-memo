9766d1df76820f5d6b92e27a05e3880e3bc59d08
8leamnk <silkviewrk@gmail.com>
2024-02-15 14:05:12 +0900

    refactor: 목표 페이지로 돌아오는지 확인하는 기능 레팩토링
    변경 내용:
    - 제네릭 타입 사용

--- src/hooks/useComeBack.tsx
+++ src/hooks/useComeBack.tsx
@@ -3,19 +3,25 @@
 import { useCallback, useEffect, useState } from 'react';
 import { HistoryInfo } from '@/constants/types';
 
-interface ComeBackProps {
-  historyInfo: HistoryInfo;
-  targetPathname: string;
+interface ComeBackProps<T> {
+  historyInfo: HistoryInfo<T>;
+  targetPathname: T;
 }
 
-function useComeBack({ historyInfo, targetPathname }: ComeBackProps): boolean {
+function useComeBack<T>({
+  historyInfo,
+  targetPathname,
+}: ComeBackProps<T>): boolean {
   const [isComeback, setIsComeback] = useState<boolean>(false);
 
-  const catchComeBack = useCallback((curHistoryInfo: HistoryInfo): boolean => {
-    const { prevPathname, currPathname } = curHistoryInfo;
+  const catchComeBack = useCallback(
+    (curHistoryInfo: HistoryInfo<T>): boolean => {
+      const { prevPathname, currPathname } = curHistoryInfo;
 
-    return !!prevPathname && currPathname === targetPathname;
-  }, []);
+      return !!prevPathname && currPathname === targetPathname;
+    },
+    [],
+  );
 
   useEffect(() => {
     const isComebackTargetPathname = catchComeBack(historyInfo);
