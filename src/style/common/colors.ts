3e6950a6b41e25cd89800aa641a5e60cee74ffef
8leamnk <silkviewrk@gmail.com>
2024-04-29 19:33:31 +0900

    feat: 입력 기능 추가

--- src/style/common/colors.ts
+++ src/style/common/colors.ts
@@ -3,6 +3,8 @@ const COLORS = {
   black: '#000',
   blazingOrange: '#ffa74f',
   honeyPeach: '#efcf8a',
+  littleBoyBlue: '#6ea1d4',
+  pinkYarrow: '#ce3375',
   pristine: '#f2e8da',
   white: '#fff',
 };
