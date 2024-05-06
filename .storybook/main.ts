35385a06dd7d134af890fc76960bbc91c5319ffc
8leamnk <silkviewrk@gmail.com>
2024-05-06 20:54:30 +0900

    feat: 스토리북 설치

---
+++ .storybook/main.ts
@@ -0,0 +1,27 @@
+import type { StorybookConfig } from '@storybook/nextjs';
+
+// 스토리북에 대한 전반적인 설정
+const config: StorybookConfig = {
+  // 스토리북에 사용할 .mdx, .stories 파일의 위치
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  // 적용할 addon
+  addons: [
+    '@storybook/addon-onboarding',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
+  ],
+  // 프레임워크 종류
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  // docs 관련
+  docs: {
+    autodocs: 'tag',
+  },
+  staticDirs: ['../public'],
+};
+
+export default config;
