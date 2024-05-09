716f2c9c238627ddb9eb887917a8490a8255dcfe
8leamnk <silkviewrk@gmail.com>
2024-05-09 20:14:42 +0900

    test: useComeback 테스트 코드 수정

--- src/hooks/__tests__/useComebackTest.tsx
+++ src/hooks/__tests__/useComebackTest.tsx
@@ -1,10 +1,15 @@
 'use client';
 
 import { act, renderHook } from '@testing-library/react';
-import ReduxProvider from '@/provider/ReduxProvider';
 import useComeBack from '../useComeBack';
 
-const useAppSelector = jest.fn();
+const mockUseAppSelector = jest.fn();
+
+jest.mock('react-redux', () => ({
+  useSelector() {
+    return mockUseAppSelector();
+  },
+}));
 
 describe('useComeBack 테스트', () => {
   test('isComeback 상태값은 목표하는 페이지로 돌아올 때 true로 바뀐다.', () => {
@@ -13,38 +18,28 @@ describe('useComeBack 테스트', () => {
     const FALSE = false;
     const TRUE = true;
     const HISTORY_FIRST = {
-      currPathname: undefined,
-      history: {},
-      prevPathname: undefined,
-    };
-    const HISTORY_SECOND = {
       currPathname: '/',
       prevPathname: undefined,
     };
-    const HISTORY_THIRD = {
+    const HISTORY_SECOND = {
       currPathname: '/about',
       prevPathname: '/',
     };
-    const HISTORY_4TH = {
+    const HISTORY_THIRD = {
       currPathname: '/my',
       prevPathname: '/about',
     };
-    const HISTORY_5TH = {
+    const HISTORY_4TH = {
       currPathname: '/',
       prevPathname: '/my',
     };
 
     // when
+    mockUseAppSelector.mockImplementation(() => ({
+      historyInfo: HISTORY_FIRST,
+    }));
     const { result, rerender } = renderHook((props) => useComeBack(props), {
       initialProps: TARGET_PATHNAME,
-      wrapper: ({ children }) => <ReduxProvider>{children}</ReduxProvider>,
-    });
-
-    act(() => {
-      useAppSelector.mockImplementation(() => ({
-        historyInfo: HISTORY_FIRST,
-      }));
-      rerender();
     });
 
     // then
@@ -52,10 +47,10 @@ describe('useComeBack 테스트', () => {
 
     // when
     act(() => {
-      useAppSelector.mockImplementation(() => ({
+      mockUseAppSelector.mockImplementation(() => ({
         historyInfo: HISTORY_SECOND,
       }));
-      rerender();
+      rerender(TARGET_PATHNAME);
     });
 
     // then
@@ -63,10 +58,10 @@ describe('useComeBack 테스트', () => {
 
     // when
     act(() => {
-      useAppSelector.mockImplementation(() => ({
+      mockUseAppSelector.mockImplementation(() => ({
         historyInfo: HISTORY_THIRD,
       }));
-      rerender();
+      rerender(TARGET_PATHNAME);
     });
 
     // then
@@ -74,21 +69,10 @@ describe('useComeBack 테스트', () => {
 
     // when
     act(() => {
-      useAppSelector.mockImplementation(() => ({
+      mockUseAppSelector.mockImplementation(() => ({
         historyInfo: HISTORY_4TH,
       }));
-      rerender();
-    });
-
-    // then
-    expect(result.current).toBe(FALSE);
-
-    // when
-    act(() => {
-      useAppSelector.mockImplementation(() => ({
-        historyInfo: HISTORY_5TH,
-      }));
-      rerender();
+      rerender(TARGET_PATHNAME);
     });
 
     // then
