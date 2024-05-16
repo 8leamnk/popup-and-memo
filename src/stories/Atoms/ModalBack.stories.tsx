41fa7844dc20984fcbf3bd2699a6afb7ec4df846
8leamnk <silkviewrk@gmail.com>
2024-05-16 20:41:10 +0900

    fix: lint error 수정

--- src/stories/Atoms/ModalBack.stories.tsx
+++ src/stories/Atoms/ModalBack.stories.tsx
@@ -33,7 +33,7 @@ export const ModalDarkBack: StoryType = {
     (Story) => (
       <>
         <main>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
+          {`Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
           scrambled it to make a type specimen book. It has survived not only
@@ -77,7 +77,7 @@ export const ModalDarkBack: StoryType = {
           words, combined with a handful of model sentence structures, to
           generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum
           is therefore always free from repetition, injected humour, or
-          non-characteristic words etc.
+          non-characteristic words etc.`}
         </main>
         <Story />
       </>
@@ -107,7 +107,7 @@ export const ModalLightBack: StoryType = {
     (Story) => (
       <>
         <main>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
+          {`Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
           scrambled it to make a type specimen book. It has survived not only
@@ -151,7 +151,7 @@ export const ModalLightBack: StoryType = {
           words, combined with a handful of model sentence structures, to
           generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum
           is therefore always free from repetition, injected humour, or
-          non-characteristic words etc.
+          non-characteristic words etc.`}
         </main>
         <Story />
       </>
