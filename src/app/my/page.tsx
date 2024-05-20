d78286de6871209f7a9c1d347c567b0704ff387e
8leamnk <silkviewrk@gmail.com>
2024-05-20 22:21:10 +0900

    feat: 마이 페이지를 서버 컴포넌트로 변경, 세션 정보를 가져오는 로직 업데이트

--- src/app/my/page.tsx
+++ src/app/my/page.tsx
@@ -1,36 +1,36 @@
-'use client';
+// 'use client';
 
 import Link from 'next/link';
-import { useSession } from 'next-auth/react';
-import styled from 'styled-components';
+// import styled from 'styled-components';
+
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/lib/authOptions';
 import Button from '@/components/Atoms/Button';
 import MemoListFeature from '@/components/Features/MemoListFeatrue';
 
-const S = {
-  Memo: styled.div`
-    width: 720px;
-    margin-top: 24px;
-  `,
-  PostButton: styled(Button)`
-    margin-bottom: 8px;
-  `,
-};
+// const S = {
+//   Memo: styled.div`
+//     width: 720px;
+//     margin-top: 24px;
+//   `,
+//   PostButton: styled(Button)`
+//     margin-bottom: 8px;
+//   `,
+// };
 
-function MyPage() {
-  const { status } = useSession();
+async function MyPage() {
+  const session = await getServerSession(authOptions);
 
-  if (status === 'authenticated') {
+  if (session) {
     return (
       <>
         <p>마이 페이지입니다.</p>
 
-        <S.Memo>
-          <Link href="my/memo/create">
-            <S.PostButton type="submit">메모 등록하기</S.PostButton>
-          </Link>
+        <Link href="my/memo/create">
+          <Button type="submit">메모 등록하기</Button>
+        </Link>
 
-          <MemoListFeature />
-        </S.Memo>
+        <MemoListFeature />
       </>
     );
   }
