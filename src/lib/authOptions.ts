b5ae5b5b5e091d2a65ddf01db739342a4673cd22
8leamnk <silkviewrk@gmail.com>
2024-05-20 22:20:15 +0900

    refactor: 서버 컴포넌트에서 사용하기 위해 로직을 분리함

---
+++ src/lib/authOptions.ts
@@ -0,0 +1,25 @@
+import { NextAuthOptions } from 'next-auth';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+import NaverProvider from 'next-auth/providers/naver';
+
+const prisma = new PrismaClient();
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    NaverProvider({
+      clientId: process.env.NAVER_CLIENT_ID || '',
+      clientSecret: process.env.NAVER_CLIENT_SECRET || '',
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, profile }) {
+      if (profile) {
+        user.name = profile.response.name || '';
+      }
+
+      return true;
+    },
+  },
+};
