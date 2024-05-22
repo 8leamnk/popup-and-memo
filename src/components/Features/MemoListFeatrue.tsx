e103372a64381a353c29334b117e60a3d438f035
8leamnk <silkviewrk@gmail.com>
2024-05-22 18:57:25 +0900

    feat: 메모 불러오는 기능에 서버 컴포넌트를 제거
    제거 이유:
    - 메모를 불러오는 컴포넌트는 메모를 등록하면 바로 반영되어야 하는 부분이기 때문

--- src/components/Features/MemoListFeatrue.tsx
+++ src/components/Features/MemoListFeatrue.tsx
@@ -1,18 +1,43 @@
-import { getServerSession } from 'next-auth';
-import { authOptions } from '@/lib/authOptions';
+'use client';
+
+import { useEffect, useState } from 'react';
+import { useSession } from 'next-auth/react';
 import { fetchMemoList } from '@/actions/memo.actions';
+import { MemoType } from '@/constants/types';
+import { LOADING_FEED_MEMO } from '@/constants/message';
 import MemoList from '../Organisms/MemoList';
 
-async function MemoListFeature() {
-  const session = await getServerSession(authOptions);
+function MemoListFeature() {
+  const { data: session, status } = useSession();
+  const [isLoading, setIsLoading] = useState(false);
+  const [memoList, setMemoList] = useState<MemoType[]>([]);
 
-  if (!!session) {
-    const memoList = await fetchMemoList(session?.user?.email);
+  const getMemoList = async () => {
+    try {
+      setIsLoading(true);
+      const data = await fetchMemoList(session?.user?.email);
+      setMemoList(data ?? []);
+      setIsLoading(false);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        console.log('message', error.message);
+      }
+      console.log('불러오기 중 오류가 발생했습니다.');
+    }
+  };
 
-    return <MemoList memoList={memoList} />;
+  useEffect(() => {
+    if (status === 'authenticated') {
+      getMemoList();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [status]);
+
+  if (isLoading) {
+    return <div>{LOADING_FEED_MEMO}</div>;
   }
 
-  return <></>;
+  return <MemoList memoList={memoList} />;
 }
 
 export default MemoListFeature;
