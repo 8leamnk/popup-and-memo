1f294c01e345ec43aa805342894dd6b7f4291086
8leamnk <silkviewrk@gmail.com>
2024-05-22 15:27:07 +0900

    test: 메모 등록하는 기능 테스트

--- src/components/Features/__tests__/CreateMemoTest.tsx
+++ src/components/Features/__tests__/CreateMemoTest.tsx
@@ -6,17 +6,19 @@ import Theme from '@/style/Theme';
 import CreateMemoFeature from '../CreateMemoFeature';
 import Popup from '@/components/Organisms/Popup';
 
-const mockUseSession = jest.fn();
+const mockGetSession = jest.fn();
 const mockPostMemo = jest.fn();
 
-jest.mock('next-auth/react', () => ({
-  useSession() {
-    return mockUseSession();
+jest.mock('@/lib/authOptions');
+
+jest.mock('next-auth', () => ({
+  getServerSession() {
+    return mockGetSession();
   },
 }));
 jest.mock('@/actions/memo.actions', () => ({
-  postMemo() {
-    return mockPostMemo();
+  postMemo(input: string, textarea: string, email: string) {
+    return mockPostMemo(input, textarea, email);
   },
 }));
 
@@ -30,7 +32,44 @@ describe('메모 등록 기능 테스트', () => {
   const SUCCESS = '등록 완료';
   const FAIL = '등록 실패';
 
-  mockUseSession.mockImplementation(() => ({ data: SESSION }));
+  mockGetSession.mockResolvedValue(SESSION);
+
+  test('입력창을 모두 입력하지 않으면 메모가 등록되지 않고 예외가 발생한다.', async () => {
+    // given
+    const TITLE_INPUT = '메모';
+    const CONTENT_INPUT = '';
+    const FAIL_MESSAGE =
+      '비어 있는 입력창이 있거나 사용자 정보에 이메일이 없어서 등록을 하지 못했습니다.';
+    const { getByText, getByLabelText } = render(
+      <ReduxProvider>
+        <StyledComponentsRegistry>
+          <Theme>
+            {await CreateMemoFeature()}
+            <Popup />
+          </Theme>
+        </StyledComponentsRegistry>
+      </ReduxProvider>,
+    );
+
+    // when
+    const titleInput = getByLabelText(TITLE);
+    const contentInput = getByLabelText(CONTENT);
+    const postButton = getByText(POST_BUTTON);
+
+    fireEvent.change(titleInput, { target: { value: TITLE_INPUT } });
+    fireEvent.change(contentInput, { target: { value: CONTENT_INPUT } });
+    fireEvent.click(postButton);
+
+    // then
+    await waitFor(
+      () => {
+        expect(mockPostMemo).toHaveBeenCalledTimes(0);
+        expect(getByText(FAIL)).toBeInTheDocument();
+        expect(getByText(FAIL_MESSAGE)).toBeInTheDocument();
+      },
+      { timeout: 100 },
+    );
+  });
 
   test('사용자 정보에 이메일이 있고, 입력창이 모두 비어있지 않으면 정상적으로 메모가 등록 된다.', async () => {
     // given
@@ -44,7 +83,7 @@ describe('메모 등록 기능 테스트', () => {
       <ReduxProvider>
         <StyledComponentsRegistry>
           <Theme>
-            <CreateMemoFeature />
+            {await CreateMemoFeature()}
             <Popup />
           </Theme>
         </StyledComponentsRegistry>
@@ -63,44 +102,13 @@ describe('메모 등록 기능 테스트', () => {
     // then
     await waitFor(
       () => {
+        expect(mockPostMemo).toHaveBeenCalledWith(
+          TITLE_INPUT,
+          CONTENT_INPUT,
+          SESSION.user.email,
+        );
         expect(getByText(SUCCESS)).toBeInTheDocument();
         expect(getByText(SUCCESS_MESSAGE)).toBeInTheDocument();
-      },
-      { timeout: 100 },
-    );
-  });
-
-  test('입력창을 모두 입력하지 않으면 메모가 등록되지 않고 예외가 발생한다.', async () => {
-    // given
-    const TITLE_INPUT = '메모';
-    const CONTENT_INPUT = '';
-    const FAIL_MESSAGE =
-      '비어 있는 입력창이 있거나 사용자 정보에 이메일이 없어서 등록을 하지 못했습니다.';
-    const { getByText, getByLabelText } = render(
-      <ReduxProvider>
-        <StyledComponentsRegistry>
-          <Theme>
-            <CreateMemoFeature />
-            <Popup />
-          </Theme>
-        </StyledComponentsRegistry>
-      </ReduxProvider>,
-    );
-
-    // when
-    const titleInput = getByLabelText(TITLE);
-    const contentInput = getByLabelText(CONTENT);
-    const postButton = getByText(POST_BUTTON);
-
-    fireEvent.change(titleInput, { target: { value: TITLE_INPUT } });
-    fireEvent.change(contentInput, { target: { value: CONTENT_INPUT } });
-    fireEvent.click(postButton);
-
-    // then
-    await waitFor(
-      () => {
-        expect(getByText(FAIL)).toBeInTheDocument();
-        expect(getByText(FAIL_MESSAGE)).toBeInTheDocument();
       },
       { timeout: 100 },
     );
