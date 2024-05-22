f30ae9a25596122d5570d6923e1b5e68a9e51a69
8leamnk <silkviewrk@gmail.com>
2024-05-22 15:26:58 +0900

    feat: 메모 등록하는 기능에 서버 컴포넌트 적용, 서버 액션을 클라이언트 컴포넌트에 props로 전달

--- src/components/Features/CreateMemoFeature.tsx
+++ src/components/Features/CreateMemoFeature.tsx
@@ -1,46 +1,22 @@
-'use client';
-
-import React from 'react';
-import { useSession } from 'next-auth/react';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/lib/authOptions';
 import { postMemo } from '@/actions/memo.actions';
-import useInput from '@/hooks/useInput';
-import useTextarea from '@/hooks/useTextarea';
-import useValidationString from '@/hooks/useValidationString';
-import usePopup from '@/hooks/usePopup';
 import CreateMemo from '@/components/Organisms/CreateMemo';
 
-function CreateMemoFeature() {
-  const { data: session } = useSession();
-  const { input, onChangeInput, resetInput } = useInput();
-  const { textarea, onChangeTextarea, resetTextarea } = useTextarea();
-  const { validateEmpty } = useValidationString();
-  const { openPopup } = usePopup();
+async function CreateMemoFeature() {
+  const session = await getServerSession(authOptions);
 
-  const onSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
+  const createMemo = async (
+    input: string,
+    textarea: string,
+  ): Promise<string> => {
+    'use server';
+    const message = await postMemo(input, textarea, session?.user?.email);
 
-    try {
-      validateEmpty([input, textarea]);
-      const message = await postMemo(input, textarea, session?.user?.email);
-      resetInput();
-      resetTextarea();
-      openPopup({ title: '등록 완료', content: message });
-    } catch (error: unknown) {
-      const content =
-        '비어 있는 입력창이 있거나 사용자 정보에 이메일이 없어서 등록을 하지 못했습니다.';
-      openPopup({ title: '등록 실패', content });
-    }
+    return message;
   };
 
-  return (
-    <CreateMemo
-      input={input}
-      textarea={textarea}
-      onChangeInput={onChangeInput}
-      onChangeTextarea={onChangeTextarea}
-      onSubmit={onSubmit}
-    />
-  );
+  return <CreateMemo createMemo={createMemo} />;
 }
 
 export default CreateMemoFeature;
