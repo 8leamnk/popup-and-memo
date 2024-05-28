a2a19ea97338ccb12d4468e2b1c80dd3d5e87353
8leamnk <silkviewrk@gmail.com>
2024-05-28 13:20:33 +0900

    refactor: 메모 가져오기 기능을 서버와 함께 서버 컴포넌트를 사용하는 것으로 변경

--- src/components/Features/SelectedMemoFeature.tsx
+++ src/components/Features/SelectedMemoFeature.tsx
@@ -1,18 +1,37 @@
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/authOptions';
-import { fetchtMemo } from '@/actions/memo.actions';
+import { PrismaClient } from '@prisma/client';
 import { MemoType } from '@/constants/types';
 import SelectedMemo from '../Organisms/SelectedMemo';
 
+const prisma = new PrismaClient();
+
 async function SelectedMemoFeature({ id }: { id: string }) {
-  const session = await getServerSession(authOptions);
-  const memo: MemoType = await fetchtMemo(id);
+  try {
+    const session = await getServerSession(authOptions);
+    const response = await prisma.memo.findUnique({
+      where: { id: Number(id) },
+    });
 
-  if (session && memo) {
-    return <SelectedMemo memo={memo} />;
+    const memo: MemoType = {
+      id: Number(response?.id),
+      createdAt: response?.createdAt?.toLocaleString() || '',
+      title: response?.title || '',
+      content: response?.content || '',
+    };
+
+    if (session) {
+      return <SelectedMemo memo={memo} />;
+    }
+
+    throw new Error('세션 정보가 없습니다.');
+  } catch (error) {
+    if (error instanceof Error) {
+      return <>{error.message}</>;
+    }
+
+    return <>불러오는 데 오류가 발생했습니다.</>;
   }
-
-  return <></>;
 }
 
 export default SelectedMemoFeature;
